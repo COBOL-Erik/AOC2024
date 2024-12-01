17cfb2de122b41746e693bfad92948cba7526b1b
S38453 <erik.weyler@seb.se>
2024-12-01 12:34:01 +0100

    1A!

--- AOC1/AOC1A.cob
+++ AOC1/AOC1A.cob
@@ -15,21 +15,23 @@
        file section.
        FD  INFIL.
        01  INDATA.
-           05  LI1            pic 9(5).
+           05  FLI1           pic 9(5).
            05  FILLER         pic X(3).
-           05  LI2            pic 9(5).
+           05  FLI2           pic 9(5).
 
        working-storage section.
        01 A-ARB.
           05 INPUT-FS         pic XX.
           05 A-COUNT          pic S9(6) comp-4 value ZERO.
+          05 IX               pic S9(6) comp-4 value ZERO.
+          05 ACC              pic S9(8) comp-4 value ZERO.
 
        01 FILLER.
-          05 LIST1 occurs 1 to 5000 times depending on A-COUNT 
-                              pic 9(5) value HIGH-VALUES.
+          05 LIST1 occurs 1 to 5000 times depending on A-COUNT.
+             10 L1            pic 9(5) value HIGH-VALUES.
        01 FILLER.
-          05 LIST2 occurs 1 to 5000 times depending on A-COUNT
-                              pic 9(5) value HIGH-VALUES.
+          05 LIST2 occurs 1 to 5000 times depending on A-COUNT.
+             10 L2            pic 9(5) value HIGH-VALUES.
 
        01 V-VAXLAR.
           05 FILLER pic X   value ' '.
@@ -46,11 +48,19 @@
            end-if
            read INFIL at end set V-INPUT-EOF to true end-read
            perform until V-INPUT-EOF
-              add 1 to A-COUNT 
-              display LI1 ' www ' LI2  
+              add 1 to A-COUNT
+              move FLI1 to LIST1(A-COUNT)
+              move FLI2 to LIST2(A-COUNT)
+      *       display LI1 ' www ' LI2  
               read INFIL at end set V-INPUT-EOF to true end-read
            end-perform
            close INFIL
+           sort LIST1 ascending key L1
+           sort LIST2 ascending key L2
+           perform varying ix from 1 by 1 until ix > A-COUNT
+              compute ACC = ACC + abs(L1(ix) - L2(ix))
+           end-perform
            display A-COUNT ' records read'
+           display ACC ' sum of differences'
            goback
            .
